a7b3e81b4509113f47f3f0a0d8acdd30adca80f4
MohamedVal <med4en@gmail.com>
2022-02-10 14:51:51 +0000

    first comit with bootstrap as dependecy and main components

---
+++ src/app/animation/reveal/reveal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reveal',
+  templateUrl: './reveal.component.html',
+  styleUrls: ['./reveal.component.css']
+})
+export class RevealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { PropertiesComponent } from './components/properties/properties.component';
+import { PropertyComponent } from './components/property/property.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RevealComponent } from './animation/reveal/reveal.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    PropertiesComponent,
+    PropertyComponent,
+    DetailsComponent,
+    ContactComponent,
+    RevealComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
