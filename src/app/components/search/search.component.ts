4d731ca974d75a8860fdc9104da4991abe238b6a
MohamedVal <med4en@gmail.com>
2022-02-12 00:45:03 +0000

    Improvement to the hero and search component UI and started implementing the admin module

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  toolTip = false;
+  min = 100000;
+  max = 1000000000;
+  minMax = this.max;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showToolTip(): void {
+    // show the akar tooltip
+    this.toolTip = !this.toolTip;
+  }
+
+  range(): void {
+    console.log("Min : " + this.min);
+    console.log("Max : " + this.max);
+
+  }
 }
